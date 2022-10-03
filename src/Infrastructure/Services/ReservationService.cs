16f2c86651290ee8fcd87966375f7626bc416632
Richard Varadi <richard_varadi@epam.com>
2022-10-03 16:26:32 +0200

    Added ReservationService /w corresponding Interface
    Interface should be injected when "Reservation to Azure Blob Storage" is required

---
+++ src/Infrastructure/Services/ReservationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services;
+internal class ReservationService : IOrderReservationService
+{
+    public Task ReserveItems(Order order)
+    {
+        throw new NotImplementedException();
+    }
+}
