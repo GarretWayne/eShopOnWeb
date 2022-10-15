1c46ec58a519d025e71c57a7cb89dc52eb9ce08f
Richard Varadi <richard_varadi@epam.com>
2022-10-15 15:16:15 +0200

    Added Dto to map to Order

---
+++ src/Infrastructure/Services/OrderDtoToReserve.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BlazorAdmin.Pages.CatalogItemPage;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services;
+
+public class OrderDtoToReserve
+{
+    public string BuyerId { get; }
+    public Address ShipToAddress { get; }
+    public List<OrderItem> ListOfItems { get; }
+    public decimal FinalPrice { get; set; }
+
+
+
+    public OrderDtoToReserve(Order order)
+    {
+        BuyerId = order.BuyerId;
+        ShipToAddress = order.ShipToAddress;
+        ListOfItems = order.OrderItems.Select(x => x).ToList();
+        FinalPrice = (from items in ListOfItems
+                      select items.UnitPrice * items.Units).Sum();
+    }
+
+}
