3c8f0695cbf36a8e0e3c342dd36487863bdc7516
Richard Varadi <richard_varadi@epam.com>
2022-10-31 13:34:01 +0100

    Added: ReservationWithServiceBusToBlob
    Next up => communicator that talks to the actual ServiceBus

---
+++ src/Infrastructure/Services/Azure/ReservationWithServiceBusToBlob.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Infrastructure.Services.Bases;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services.Azure;
+
+internal class ReservationWithServiceBusToBlob : ReservationServiceBase
+{
+    public ReservationWithServiceBusToBlob(IConfiguration configuration, IAppLogger<ReservationWithAzureFunctionToCosmosDb> logger, IAzureCommunicatorService communicator, IOrderRequestContentAssembler assembler, ISecretBrokerService secretBrokerService) : base(configuration, logger, communicator, assembler, secretBrokerService)
+    {
+    }
+
+    protected override async Task SendOrderAsync(string content)
+    {
+        RequestTargetUri =
+            await RetrieveRequestTargetUri("SecretNames:ReservationWithServiceBusToBlob");
+        await _communicator.SendOrderRequestAsync(content, RequestTargetUri);
+    }
+}
