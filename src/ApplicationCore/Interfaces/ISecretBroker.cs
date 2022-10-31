1cb3ff0e955bee24cf87d70ad83585348aff648c
Richard Varadi <richard_varadi@epam.com>
2022-10-31 12:10:51 +0100

    Added SecretBroker for KeyVault Secret Retrieving
    Can be injected with the ISecretBroker interface

---
+++ src/ApplicationCore/Interfaces/ISecretBroker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services;
+
+public interface ISecretBroker
+{
+    Task<String> GetSecretAsStringByNameAsync(string secretName);
+}
---
+++ src/Infrastructure/Services/KeyVaultSecretBroker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services;
+
+internal class KeyVaultSecretBroker : ISecretBroker
+{
+    private readonly IConfiguration _configuration;
+    private readonly IAppLogger<KeyVaultSecretBroker> _logger;
+    private readonly SecretClient _secretClient;
+
+    public KeyVaultSecretBroker(IConfiguration configuration, IAppLogger<KeyVaultSecretBroker> logger)
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _secretClient = SetupSecretClient(_configuration);
+    }
+
+    private SecretClient SetupSecretClient(IConfiguration configuration)
+    {
+        try
+        {
+            _logger.LogInformation("Start - KeyVaultClient");
+
+            string keyVaultName = configuration["SecretNames:KeyVault"];
+            var keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+
+            var secretClient = new SecretClient(new Uri(keyVaultUri),
+                new DefaultAzureCredential());
+
+
+            _logger.LogInformation("DONE - KeyVault Connected");
+            return secretClient;
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical($"FAILED - KeyVault Connection Setup + {e}");
+            throw;
+        }
+    }
+
+    public async Task<string> GetSecretAsStringByNameAsync(string secretName)
+    {
+        var result = await _secretClient.GetSecretAsync(secretName);
+        return result.Value.Value;
+    }
+}
