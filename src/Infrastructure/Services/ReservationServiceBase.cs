2b7ce31cf6d3b38e5e17dba5c7d385329ff07b36
Richard Varadi <richard_varadi@epam.com>
2022-10-31 12:19:00 +0100

    BASE EXTRACT: Extracted ReservationServiceBase
    Goal => better maintainability

---
+++ src/Infrastructure/Services/ReservationServiceBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Infrastructure.Dtos;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.eShopWeb.Infrastructure.Services;
+
+public abstract class ReservationServiceBase : IOrderReservationService
+{
+    protected IConfiguration _configuration;
+    protected IAppLogger<ReservationWithCosmosDbService> _logger;
+    protected IAzureOrderReservationCommunicatorService _orderReservationCommunicator;
+    protected IOrderContentAssembler _assembler;
+    protected ISecretBroker _secretBroker;
+
+    protected ReservationServiceBase
+    (
+        IConfiguration configuration,
+        IAppLogger<ReservationWithCosmosDbService> logger,
+        IAzureOrderReservationCommunicatorService orderReservationCommunicator,
+        IOrderContentAssembler assembler,
+        ISecretBroker secretBroker
+        )
+    {
+        _configuration = configuration;
+        _logger = logger;
+        _orderReservationCommunicator = orderReservationCommunicator;
+        _assembler = assembler;
+        _secretBroker = secretBroker;
+    }
+
+    public async Task ReserveItems(Order order)
+    {
+        try
+        {
+            _logger.LogInformation("START - Order reservation to be sent");
+
+            var orderContent = await AssembleRequestContent(order);
+            await SendOrderAsync(orderContent);
+
+            _logger.LogInformation("END - Order has been sent");
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical($"FAILED - Reservation Of Items failed -{e}");
+            throw;
+        }
+    }
+
+    protected async Task<string> AssembleRequestContent(Order order)
+    {
+        try
+        {
+            _logger.LogInformation("START - Request Content Building");
+
+            var dtoToJsonify = new OrderDtoToReserve(order);
+
+            _logger.LogInformation("PART - Dto -- Dto Assembled");
+
+            var result = await _assembler.AssembleRequestContentAsync(dtoToJsonify);
+
+            _logger.LogInformation("DONE - Request Content Building");
+            return result;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError($"FAILED - Assembling the RequestContent -{e}");
+            throw;
+        }
+    }
+
+    protected abstract Task SendOrderAsync(string content);
+
+    protected async Task<string> RetrieveRequestTargetUri(string appSettingsSecretName)
+    {
+        return await _secretBroker.GetSecretAsStringByNameAsync(_configuration[appSettingsSecretName]);
+    }
+}
--- src/Infrastructure/Services/ReservationWithCosmosDbService.cs
+++ src/Infrastructure/Services/ReservationWithCosmosDbService.cs
@@ -1,60 +1,30 @@
-﻿using System;
-using System.Threading.Tasks;
-using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
+﻿using System.Threading.Tasks;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.Infrastructure.Dtos;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.eShopWeb.Infrastructure.Services;
 
-public class ReservationWithCosmosDbService : IOrderReservationService
+public class ReservationWithCosmosDbService : ReservationServiceBase
 {
-    private readonly IConfiguration _configuration;
-    private readonly IAppLogger<ReservationWithCosmosDbService> _logger;
-    private readonly IAzureOrderReservationCommunicatorService _orderReservationCommunicator;
-    private readonly IOrderContentAssembler _assembler;
-
-    public ReservationWithCosmosDbService(IConfiguration configuration, IAppLogger<ReservationWithCosmosDbService> logger, IAzureOrderReservationCommunicatorService orderReservationCommunicator, IOrderContentAssembler assembler)
+    public ReservationWithCosmosDbService(IConfiguration configuration,
+        IAppLogger<ReservationWithCosmosDbService> logger,
+        IAzureOrderReservationCommunicatorService orderReservationCommunicator,
+        IOrderContentAssembler assembler,
+        ISecretBroker secretBroker
+    ) : base(configuration, logger, orderReservationCommunicator, assembler, secretBroker)
     {
-        _configuration = configuration;
-        _logger = logger;
-        _orderReservationCommunicator = orderReservationCommunicator;
-        _assembler = assembler;
+
     }
 
-    public async Task ReserveItems(Order order)
+    protected override async Task SendOrderAsync(string content)
     {
-        _logger.LogInformation("START - Order reservation to be sent");
+        string requestTargetUri = await RetrieveRequestTargetUri("SecretNames:OrderReserveAzureFunc");
 
-        var orderContent = await AssembleOrderContent(order);
-        await SendOrderAsync(orderContent);
-
-        _logger.LogInformation("END - Order has been sent");
+        await _orderReservationCommunicator.OnSendOrderAsync(content, requestTargetUri);
     }
 
-    private async Task<string> AssembleOrderContent(Order order)
-    {
-        var dtoToJsonify = new OrderDtoToReserve(order);
-        return await _assembler.AssembleOrderContentAsync(dtoToJsonify);
-    }
-
-    public async Task SendOrderAsync(string content)
-    {
-        _logger.LogInformation("Start - KeyVault");
-        string keyVaultName = "keyvaulttestazuretest";
-        var keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
-
-        var secretClient = new SecretClient(new Uri(keyVaultUri),
-            new DefaultAzureCredential());
-        var secretUriResponse = await secretClient.GetSecretAsync("ConnectionStrings--AzureFunc--OrderReserve");
-
-        _logger.LogInformation("DONE - KeyVault");
-
-
-
-        string requestUri = secretUriResponse.Value.Value;
-        await _orderReservationCommunicator.OnSendOrderAsync(content, requestUri);
-    }
+    
 }
