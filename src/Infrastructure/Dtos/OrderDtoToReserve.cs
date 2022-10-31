86282b556e1b3e266ff39c0cc4d88a7f4386181f
Richard Varadi <richard_varadi@epam.com>
2022-10-31 12:12:24 +0100

    Refactored OrderDto into RECORD
    Thus making the values immutable

--- src/Infrastructure/Dtos/OrderDtoToReserve.cs
+++ src/Infrastructure/Dtos/OrderDtoToReserve.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BlazorAdmin.Pages.CatalogItemPage;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 
 namespace Microsoft.eShopWeb.Infrastructure.Dtos;
 
-public class OrderDtoToReserve
+public record OrderDtoToReserve
 {
-    public string BuyerId { get; set; }
-    public Address ShipToAddress { get; set; }
-    public List<OrderItem> ListOfItems { get; set; }
-    public decimal FinalPrice { get; set; }
+    public string BuyerId { get; init; }
+    public Address ShipToAddress { get; init; }
+    public List<OrderItem> ListOfItems { get; init; }
+    public decimal FinalPrice { get; init; }
 
+    [Obsolete(
+        $"Should not be able to initialize Record without an actual Order. Azure Functions should NOT know what an Order is, and should NOT KNOW of this class AT ALL")]
     public OrderDtoToReserve()
     {
 
@@ -19,6 +22,7 @@ public class OrderDtoToReserve
 
     public OrderDtoToReserve(Order order)
     {
+
         BuyerId = order.BuyerId;
         ShipToAddress = order.ShipToAddress;
         ListOfItems = order.OrderItems.Select(x => x).ToList();
